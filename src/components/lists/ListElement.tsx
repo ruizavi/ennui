76bc78618bc01501260417d5cbea294fc394dab3
avi <avi.ruiz@proton.me>
2024-03-18 19:14:00 -0600

    upd: only button to move list

--- src/components/lists/ListElement.tsx
+++ src/components/lists/ListElement.tsx
@@ -1,6 +1,7 @@
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import { List } from "@prisma/client";
+import { MoveIcon } from "../icons/MoveIcon";
 
 export const ListElement = (props: List) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
@@ -12,16 +13,22 @@ export const ListElement = (props: List) => {
   };
 
   return (
-    <li
-      style={style}
-      {...attributes}
-      {...listeners}
-      ref={setNodeRef}
-      className="w-[270px] bg-white bg-opacity-30 rounded-lg"
-    >
-      <h1 className="text-center bg-white bg-opacity-50 rounded-t-lg py-2 font-semibold">
-        {props.name}
-      </h1>
+    <li style={style} className="w-[270px] bg-white bg-opacity-30 rounded-lg">
+      <header className="flex justify-between bg-white bg-opacity-50 rounded-t-lg">
+        <h1 className="text-center py-2 font-semibold w-full">{props.name}</h1>
+        <button
+          ref={setNodeRef}
+          {...attributes}
+          {...listeners}
+          className="group px-4"
+        >
+          <MoveIcon
+            height={16}
+            width={16}
+            className="fill-[#C11D5A] group-hover:fill-[#CD4A7B]"
+          />
+        </button>
+      </header>
     </li>
   );
 };
